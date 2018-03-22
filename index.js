9fea20d5baec48318881126a8aff73ac46170c1f
Joakim Tysseng <joakim@tysseng.no>
2018-03-22 16:27:47 +0100

    Bugs

--- index.js
+++ index.js
@@ -6,17 +6,17 @@
  -----------------------------------------------------------------------------
  Author: Poltorak Serguei <ps@z-wave.me>
  Description:
-	 Log sensor value in JSON file
+ Log sensor value in JSON file
 
-******************************************************************************/
+ ******************************************************************************/
 
 // ----------------------------------------------------------------------------
 // --- Class definition, inheritance and setup
 // ----------------------------------------------------------------------------
 
-function PowerSensorWebhook (id, controller) {
-	// Call superconstructor first (AutomationModule)
-	PowerSensorWebhook.super_.call(this, id, controller);
+function PowerSensorWebhook(id, controller) {
+  // Call superconstructor first (AutomationModule)
+  PowerSensorWebhook.super_.call(this, id, controller);
 };
 
 inherits(PowerSensorWebhook, AutomationModule);
@@ -28,14 +28,14 @@ _module = PowerSensorWebhook;
 // ----------------------------------------------------------------------------
 
 PowerSensorWebhook.prototype.init = function (config) {
-	console.log("Kickstarted Power Sensor Webhook");
-	// Call superclass' init (this will process config argument and so on)
-	PowerSensorWebhook.super_.prototype.init.call(this, config);
+  console.log("Kickstarted Power Sensor Webhook");
+  // Call superclass' init (this will process config argument and so on)
+  PowerSensorWebhook.super_.prototype.init.call(this, config);
 
-	// Remember "this" for detached callbacks (such as event listener callbacks)
-	var self = this;
+  // Remember "this" for detached callbacks (such as event listener callbacks)
+  var self = this;
 
-	this.handler = function (vDev) {
+  this.handler = function (vDev) {
     var id = vDev.id;
     var value = vDev.get('metrics:level');
 
@@ -45,40 +45,41 @@ PowerSensorWebhook.prototype.init = function (config) {
     var tresholdLow = self.config.tresholdLow || 0;
     var tresholdHigh = self.config.tresholdHigh || 0.1;
 
-		if(urlChange && urlChange !== ''){
-			console.log("Sensor changed, updating: " + id + '=' + value);
-			http.request({
-				method: 'GET',
-				url: urlChange.replace("${id}", id).replace("${value}", value )
-			});
-		}
-		if(urlOn && urlOn !== ''){
-			if(value >= tresholdHigh){
+    if (urlChange && urlChange !== '') {
+      console.log("Sensor changed, updating: " + id + '=' + value);
+      http.request({
+        method: 'GET',
+        url: urlChange.replace("${id}", id).replace("${value}", value)
+      });
+    }
+    if (urlOn && urlOn !== '') {
+      if (value >= tresholdHigh) {
         console.log("Sensor value is on, updating: " + id + '=' + value);
         http.request({
           method: 'GET',
-          url: urlOn.replace("${id}", id).replace("${value}", value )
+          url: urlOn.replace("${id}", id).replace("${value}", value)
         });
-			}
-		}
-		if(urlOff && urlOff !== ''){
-      if(value <= tresholdLow){
-			console.log("Sensor value is off, updating: " + id + '=' + value);
-			http.request({
-				method: 'GET',
-				url: urlOff.replace("${id}", id).replace("${value}", value )
-			});
-		}
-	};
+      }
+    }
+    if (urlOff && urlOff !== '') {
+      if (value <= tresholdLow) {
+        console.log("Sensor value is off, updating: " + id + '=' + value);
+        http.request({
+          method: 'GET',
+          url: urlOff.replace("${id}", id).replace("${value}", value)
+        });
+      }
+    }
+  };
 
-	// Setup metric update event listener
-	this.controller.devices.on(this.config.device, "change:metrics:level", this.handler);
+  // Setup metric update event listener
+  this.controller.devices.on(this.config.device, "change:metrics:level", this.handler);
 };
 
 PowerSensorWebhook.prototype.stop = function () {
-	PowerSensorWebhook.super_.prototype.stop.call(this);
+  PowerSensorWebhook.super_.prototype.stop.call(this);
 
-	this.controller.devices.off(this.config.device, "change:metrics:level", this.handler);
+  this.controller.devices.off(this.config.device, "change:metrics:level", this.handler);
 };
 
 // ----------------------------------------------------------------------------
