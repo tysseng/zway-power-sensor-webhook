8a2176200ee644f0dff082a1d22cce4fe7493221
Joakim Tysseng <joakim.tysseng@bekk.no>
2018-03-22 14:04:27 +0000

    First commit, cloned SensorValueLogging module

---
+++ index.js
@@ -0,0 +1,76 @@
+/*** PowerSensorWebhook Z-Way Home Automation module *************************************
+
+ Version: 1.0.1
+ (c) Z-Wave.Me, 2014
+
+ -----------------------------------------------------------------------------
+ Author: Poltorak Serguei <ps@z-wave.me>
+ Description:
+	 Log sensor value in JSON file
+
+******************************************************************************/
+
+// ----------------------------------------------------------------------------
+// --- Class definition, inheritance and setup
+// ----------------------------------------------------------------------------
+
+function PowerSensorWebhook (id, controller) {
+	// Call superconstructor first (AutomationModule)
+	PowerSensorWebhook.super_.call(this, id, controller);
+};
+
+inherits(PowerSensorWebhook, AutomationModule);
+
+_module = PowerSensorWebhook;
+
+// ----------------------------------------------------------------------------
+// --- Module instance initialized
+// ----------------------------------------------------------------------------
+
+PowerSensorWebhook.prototype.init = function (config) {
+console.log("Kickstarted Sensor Value Logging");
+	// Call superclass' init (this will process config argument and so on)
+	PowerSensorWebhook.super_.prototype.init.call(this, config);
+
+	// Remember "this" for detached callbacks (such as event listener callbacks)
+	var self = this;
+
+	this.handler = function (vDev) {
+		if (self.config.logTo === "JSONFile") {
+			var storedLog = loadObject("PowerSensorWebhook_" + vDev.id + "_" + self.id);
+			if (!storedLog) {
+				storedLog = {
+					deviceId: vDev.id,
+					deviceName: vDev.get("metrics:title"),
+					sensorData: []
+				};
+			}
+			storedLog.sensorData.push({"time": Date.now(), "value": vDev.get("metrics:level")});
+			saveObject("PowerSensorWebhook_" + vDev.id + "_" + self.id, storedLog);
+			storedLog = null;
+		}
+		
+		if (self.config.logTo === "HTTPGET") {
+			console.log('SVL: Saving to HTTP - dev: ' + vDev.id + ', val:' + vDev.get('metrics:level'));
+			http.request({
+				method: 'GET',
+				url: self.config.url.replace("${id}", vDev.id).replace("${value}", vDev.get('metrics:level'))
+			});
+		}
+	};
+
+	// Setup metric update event listener
+	this.controller.devices.on(this.config.device, "change:metrics:level", this.handler);
+};
+
+PowerSensorWebhook.prototype.stop = function () {
+	PowerSensorWebhook.super_.prototype.stop.call(this);
+
+	this.controller.devices.off(this.config.device, "change:metrics:level", this.handler);
+};
+
+// ----------------------------------------------------------------------------
+// --- Module methods
+// ----------------------------------------------------------------------------
+
+// This module doesn't have any additional methods
